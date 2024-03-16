de893f3778920a881fd91db8b3f5235371877a3e
Swapnil-197 <sspatil1907@gmail.com>
2024-03-16 17:40:20 +0530

    added code related to exception

--- SpringBoot_05_ExceptionHandling/src/main/java/com/sb/service/StudentService.java
+++ SpringBoot_05_ExceptionHandling/src/main/java/com/sb/service/StudentService.java
@@ -2,11 +2,10 @@ package com.sb.service;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.sb.dao.StudentDao;
+import com.sb.exceptions.StudentNotFoundException;
 import com.sb.pojo.Student;
 
 @Service
@@ -29,7 +28,13 @@ public class StudentService {
 	}
 
 	public Optional<Student> getStudentById(Integer studentid) {
-		return studentDao.findById(studentid);
+		Optional<Student> student = studentDao.findById(studentid);
+		if(student.isPresent()) {
+			return student;
+		}else { 
+			throw new StudentNotFoundException("Student not found in the database : " + studentid);
+		  // Here we are not returning Optional Object we want to return Exception in JSON format.
+		}
 	}
 
 	public void deleteStudentById(Integer studentId) {
